cd8c95879445ba01ff5b8c5033a5d598d2db4eee
BC. MICHAL MISKOLCI <misoblack300@gmail.com>
2025-03-19 15:22:30 +0100

    Publish and Subscribe

--- assignments/lab_03/lab03_ws/src/my_pkg/src/publisherNode.cpp
+++ assignments/lab_03/lab03_ws/src/my_pkg/src/publisherNode.cpp
@@ -17,29 +17,41 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-
+#include "std_msgs/msg/float32.hpp"
+using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class PubSubNode : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
+  PubSubNode()
   : Node("myNode"), count_(0)
   {
+    // Publisher definition
     publisher_ = this->create_publisher<std_msgs::msg::String>("node_name", 10);
+    
+    // Subscriber definition
+    subscriber_ = this->create_subscription<std_msgs::msg::Float32>("battery_voltage",10, 			std::bind(&PubSubNode::topic_callback, this, _1));
+    
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&PubSubNode::timer_callback, this));
   }
 
 private:
+  void topic_callback(const std_msgs::msg::Float32::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "%f", msg->data);
+  }
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscriber_;
+  
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
     message.data = "myNode";
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    RCLCPP_INFO(this->get_logger(), message.data.c_str());
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
@@ -50,7 +62,7 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<PubSubNode>());
   rclcpp::shutdown();
   return 0;
 }
